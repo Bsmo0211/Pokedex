ff76b706a524551063715734162271a4f76b7fa7
Bsmo0211 <bsmartinezo@geosat.com.co>
2026-03-02 17:21:51 -0500

    se coloca el gesture detecto para la redireccion

--- lib/features/pokedex/presentation/widgets/pokemon_card.dart
+++ lib/features/pokedex/presentation/widgets/pokemon_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pokedex/features/pokedex/domain/entities/pokemon.dart';
 
 class PokemonCard extends StatelessWidget {
@@ -8,87 +9,90 @@ class PokemonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 2,
-      margin: const EdgeInsets.symmetric(vertical: 4),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      child: SizedBox(
-        height: 110,
-        child: Row(
-          children: [
-            Container(
-              width: 100,
-              height: 110,
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(15),
-                  bottomLeft: Radius.circular(15),
+    return GestureDetector(
+      onTap: () => context.push('/home/pokemon/${pokemon.name}'),
+      child: Card(
+        elevation: 2,
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: SizedBox(
+          height: 110,
+          child: Row(
+            children: [
+              Container(
+                width: 100,
+                height: 110,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    bottomLeft: Radius.circular(15),
+                  ),
+                ),
+                child: Image.network(
+                  pokemon.imageUrl,
+                  fit: BoxFit.contain,
+                  errorBuilder: (context, error, stackTrace) =>
+                      const Icon(Icons.error),
                 ),
               ),
-              child: Image.network(
-                pokemon.imageUrl,
-                fit: BoxFit.contain,
-                errorBuilder: (context, error, stackTrace) =>
-                    const Icon(Icons.error),
-              ),
-            ),
 
-            const SizedBox(width: 16),
+              const SizedBox(width: 16),
 
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '#${pokemon.id.toString().padLeft(3, '0')}',
-                    style: const TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '#${pokemon.id.toString().padLeft(3, '0')}',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Text(
-                    pokemon.name.toUpperCase(),
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                    Text(
+                      pokemon.name.toUpperCase(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8),
-                  // Los tipos
-                  Wrap(
-                    spacing: 4,
-                    children: pokemon.types
-                        .map(
-                          (type) => Container(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 2,
-                            ),
-                            decoration: BoxDecoration(
-                              color: _getTypeColor(type),
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                            child: Text(
-                              type,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 11,
+                    const SizedBox(height: 8),
+                    // Los tipos
+                    Wrap(
+                      spacing: 4,
+                      children: pokemon.types
+                          .map(
+                            (type) => Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 2,
+                              ),
+                              decoration: BoxDecoration(
+                                color: _getTypeColor(type),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              child: Text(
+                                type,
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 11,
+                                ),
                               ),
                             ),
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ],
+                          )
+                          .toList(),
+                    ),
+                  ],
+                ),
               ),
-            ),
 
-            const Icon(Icons.chevron_right, color: Colors.grey),
-            const SizedBox(width: 10),
-          ],
+              const Icon(Icons.chevron_right, color: Colors.grey),
+              const SizedBox(width: 10),
+            ],
+          ),
         ),
       ),
     );
