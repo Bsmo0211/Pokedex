db6e552a182da07a39d3bea81b521233fd83b8fd
Bsmo0211 <bsmartinezo@geosat.com.co>
2026-03-02 17:54:33 -0500

    se quita color blanco para funcionalidad de modos visuales

--- lib/features/onboarding/presentation/screen/onboarding_screen.dart
+++ lib/features/onboarding/presentation/screen/onboarding_screen.dart
@@ -15,7 +15,6 @@ class OnboardingScreen extends ConsumerWidget {
     PageController pageController = PageController();
 
     return Scaffold(
-      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           children: [
