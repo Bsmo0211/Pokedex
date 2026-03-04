904b1718741a92197ad5f00389183a9473c572cd
Bsmo0211 <bsmartinezo@geosat.com.co>
2026-03-04 16:01:06 -0500

    arreglo de boton favorito

--- lib/features/pokedex/presentation/widgets/favorite_button.dart
+++ lib/features/pokedex/presentation/widgets/favorite_button.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/features/favorites/presentation/providers/pokemon_favorites_provider.dart';
+import 'package:pokedex/features/pokedex/domain/entities/pokemon.dart';
 
-class FavoriteButton extends StatelessWidget {
-  const FavoriteButton({super.key});
+class FavoriteButton extends ConsumerWidget {
+  final Pokemon pokemon;
+
+  const FavoriteButton({super.key, required this.pokemon});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    bool isFavorite = ref
+        .watch(pokemonFavoritesProvider)
+        .any((p) => p.id == pokemon.id);
+
     return Positioned(
       top: 12,
       right: 12,
-      child: Container(
-        padding: const EdgeInsets.all(4),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: const Color.fromARGB(125, 29, 25, 25),
-          border: Border.all(color: Colors.white54, width: 1.5),
+      child: GestureDetector(
+        onTap: () {
+          ref.read(pokemonFavoritesProvider.notifier).toggleFavorite(pokemon);
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          padding: const EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: const Color.fromARGB(125, 29, 25, 25),
+            border: Border.all(color: Colors.white54, width: 1.5),
+          ),
+          child: Icon(
+            isFavorite ? Icons.favorite : Icons.favorite_border,
+            color: isFavorite ? Colors.red : Colors.white,
+            size: 16,
+          ),
         ),
-        child: const Icon(Icons.favorite_border, color: Colors.white, size: 16),
       ),
     );
   }
