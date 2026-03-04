acb793fa7dff219e616acf1af7aa64262c487975
Bsmo0211 <bsmartinezo@geosat.com.co>
2026-03-04 16:00:32 -0500

    parametro de pokemon

--- lib/features/pokedex/presentation/widgets/pokemon_card.dart
+++ lib/features/pokedex/presentation/widgets/pokemon_card.dart
@@ -73,7 +73,7 @@ class _PokemonCardState extends State<PokemonCard> {
                   isHovered: _isHovered,
                 ),
 
-                const FavoriteButton(),
+                FavoriteButton(pokemon: widget.pokemon),
               ],
             ),
           ),
