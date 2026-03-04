ffa292fd957599b338ecd662c2f566dd04d393b2
Bsmo0211 <bsmartinezo@geosat.com.co>
2026-03-04 13:28:35 -0500

    se crea constante para svg path

--- lib/core/utils/assets_constants.dart
+++ lib/core/utils/assets_constants.dart
@@ -1,5 +1,6 @@
 class AssetsManager {
-  static const String _imagesPath = 'assets/images/';
-  static const String onboarding1 = '${_imagesPath}onboarding_1.png';
-  static const String onboarding2 = '${_imagesPath}onboarding_2.png';
+  static const String imagesPath = 'assets/images/';
+  static const String onboarding1 = '${imagesPath}onboarding_1.png';
+  static const String onboarding2 = '${imagesPath}onboarding_2.png';
+  static const String svgPath = 'assets/svg/Property 1=';
 }
