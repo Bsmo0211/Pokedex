8fd7b1a537469c4355fd4ec746d49a7ee484101f
Bsmo0211 <bsmartinezo@geosat.com.co>
2026-03-04 14:47:26 -0500

    re ajuste de funciones

--- lib/features/pokedex/presentation/screens/pokedex_screen.dart
+++ lib/features/pokedex/presentation/screens/pokedex_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pokedex/features/pokedex/domain/entities/pokemon.dart';
 import 'package:pokedex/features/pokedex/presentation/providers/pokemon_list_provider.dart';
+import 'package:pokedex/features/pokedex/presentation/widgets/filter_bar.dart';
 import 'package:pokedex/features/pokedex/presentation/widgets/pokedex_error.dart';
 import 'package:pokedex/features/pokedex/presentation/widgets/pokemon_card.dart';
+import 'package:pokedex/l10n/app_localizations.dart';
 
 final isManualLoadingProvider = StateProvider<bool>((ref) => false);
 
@@ -16,33 +19,55 @@ class PokedexScreen extends ConsumerStatefulWidget {
 class _PokedexScreenState extends ConsumerState<PokedexScreen> {
   final ScrollController _scrollController = ScrollController();
   bool _isFetching = false;
+  String searchQuery = "";
+  final List<String> selectedTypes = [];
+
+  List<Pokemon> _getFilteredPokemon(List<Pokemon> allPokemon) {
+    return allPokemon.where((pokemon) {
+      // Filtro por nombre
+      final bool matchesName = pokemon.name.toLowerCase().contains(
+        searchQuery.toLowerCase(),
+      );
+
+      // Filtro por tipo
+      final bool matchesType =
+          selectedTypes.isEmpty ||
+          pokemon.types.any((t) => selectedTypes.contains(t.toLowerCase()));
+
+      return matchesName && matchesType;
+    }).toList();
+  }
+
+  void _onScroll() async {
+    if (_scrollController.position.pixels >=
+            _scrollController.position.maxScrollExtent - 300 &&
+        !_isFetching) {
+      _isFetching = true;
+      await ref.read(pokemonListProvider.notifier).fetchNextPage();
+      if (mounted) {
+        _isFetching = false;
+      }
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(() async {
-      if (_scrollController.position.pixels >=
-              _scrollController.position.maxScrollExtent - 300 &&
-          !_isFetching) {
-        _isFetching = true;
-
-        await ref.read(pokemonListProvider.notifier).fetchNextPage();
-
-        _isFetching = false;
-      }
-    });
+    _scrollController.addListener(_onScroll);
   }
 
   @override
   void dispose() {
+    _scrollController.removeListener(_onScroll);
     _scrollController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final pokemonListAsync = ref.watch(pokemonListProvider);
-    final isManualLoading = ref.watch(isManualLoadingProvider);
+    AppLocalizations l10n = AppLocalizations.of(context)!;
+    AsyncValue<List<Pokemon>> pokemonListAsync = ref.watch(pokemonListProvider);
+    bool isManualLoading = ref.watch(isManualLoadingProvider);
 
     if (isManualLoading) {
       return const Scaffold(
@@ -52,50 +77,42 @@ class _PokedexScreenState extends ConsumerState<PokedexScreen> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Pokédex',
-          style: TextStyle(fontWeight: FontWeight.bold),
+        title: Text(
+          l10n.pokedexTitle,
+          style: const TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        elevation: 0,
       ),
       body: pokemonListAsync.when(
         data: (pokemonList) {
-          return ListView.builder(
-            controller: _scrollController,
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-            itemCount: pokemonList.length + 1,
-            itemBuilder: (context, index) {
-              if (index < pokemonList.length) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 12),
-                  child: PokemonCard(pokemon: pokemonList[index]),
-                );
-              } else {
-                return const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 32),
-                  child: Center(
-                    child: CircularProgressIndicator(strokeWidth: 2),
-                  ),
-                );
-              }
-            },
+          List<Pokemon> filteredList = _getFilteredPokemon(pokemonList);
+
+          return Column(
+            children: [
+              FilterBar(
+                searchQuery: searchQuery,
+                selectedTypes: selectedTypes,
+                onFiltersChanged: () {
+                  setState(() {});
+                },
+                onSearchChanged: (value) {
+                  setState(() => searchQuery = value);
+                },
+              ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  itemCount: filteredList.length,
+                  itemBuilder: (context, index) =>
+                      PokemonCard(pokemon: filteredList[index]),
+                ),
+              ),
+            ],
           );
         },
-        loading: () =>
-            const Center(child: CircularProgressIndicator(strokeWidth: 3)),
+        loading: () => const Center(child: CircularProgressIndicator()),
         error: (err, stack) => PokedexErrorWidget(
-          onRetry: () async {
-            ref.read(isManualLoadingProvider.notifier).state = true;
-            try {
-              ref.invalidate(pokemonListProvider);
-              await ref.read(pokemonListProvider.future);
-            } catch (e) {
-              debugPrint("Error: $e");
-            } finally {
-              ref.read(isManualLoadingProvider.notifier).state = false;
-            }
-          },
+          onRetry: () => ref.invalidate(pokemonListProvider),
         ),
       ),
     );
